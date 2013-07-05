26c4667bdab847bae1b0f45b198e3cdc7c019068
Ge Tan <gtan@me.com>
2013-07-05 12:03:38 +0100

    update the readXls

--- scripts/IO.r
+++ scripts/IO.r
@@ -5,7 +5,17 @@ readXls = function(xlsFn, sheet="Sheet1", chargeState){
   sheetData = readWorksheet(wb, sheet=sheet)
   indexMain = which(!is.na(sheetData[ ,"No.."]))
   resultSheet = sheetData[indexMain, ]
-  indexChargeState = which(sheetData[ ,"Average.Mass"] == chargeState)
+  #indexChargeState = which(sheetData[ ,"Average.Mass"] == chargeState)
+  indexChargeState = rep(NA, nrow(resultSheet))
+  j = 0
+  for(i in 1:nrow(sheetData)){
+    if(sheetData[i, "Average.Mass"] == "Charge State"){
+      j = j + 1
+    }
+    if(sheetData[i, "Average.Mass"] == as.character(chargeState)){
+      indexChargeState[j] = i
+    }
+  }
   measuredAverage = sheetData[indexChargeState, "Sum.Intensity"]
   stopifnot(length(measuredAverage) == nrow(resultSheet))
   resultSheet = cbind(resultSheet, "Measured Average m/z"=as.numeric(measuredAverage))
